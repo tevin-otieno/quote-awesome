da958b89e547d21fd4e1f1f628c6bd4c1c555cae
tevin-otieno <tevin.otieno@student.moringaschool.com>
2021-10-10 12:19:44 +0300

    added the quote class to be intergrated in the application

---
+++ src/app/quote.ts
@@ -0,0 +1,6 @@
+export class Quote {
+
+    constructor(public id:number, public name:string, public quote:string, public author:string, public date:Date, public inspiration:number, public terrible:number){
+
+    }
+}
