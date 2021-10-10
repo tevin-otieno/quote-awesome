37a712f5ead54597b8f179165f5b9d1f8f666e99
tevin-otieno <tevin.otieno@student.moringaschool.com>
2021-10-10 21:51:45 +0300

    gabage collection and reviewing my code to make them work

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QuoteFormComponent } from './quote-form/quote-form.component';
 import { QuoteListComponent } from './quote-list/quote-list.component';
-import { HighlightQuoteDirective } from './highlight-quote.directive';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { DatePipe } from './date.pipe';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -16,7 +14,6 @@ import { FormsModule } from '@angular/forms';
     AppComponent,
     QuoteFormComponent,
     QuoteListComponent,
-    HighlightQuoteDirective,
     DatePipe
   ],
   imports: [
--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,10 +1,9 @@
 export class Quote {
-    public inspiration;
-    public terrible;
+    public inspiration:number;
+    public terrible:number;
 
     constructor(public id:number, public name:string, public quote:string, public author:string, public date:Date){
         this.inspiration=0;
         this.terrible=0;
-
     }
 }
